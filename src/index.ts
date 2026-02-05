c7caa775cabef57ac0dc0eece31d6daa33481162
Sabyasachi1996 <tripathy014@gmail.com>
2026-02-05 17:58:23 +0530

    first commit

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req: Request, res: Response) => {
+    console.log('application hit');
     res.send('Hello from TypeScript Express!');
 });
 
