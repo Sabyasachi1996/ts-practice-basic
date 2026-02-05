12fae42bb081af2a36d961940ad0922bd3f943b1
Sabyasachi1996 <tripathy014@gmail.com>
2026-02-05 23:17:45 +0530

    demo commit2

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req: Request, res: Response) => {
-    console.log('application hit with small change');
+    console.log('application hit with small change c');
     res.send('Hello from TypeScript Express!');
 });
 
